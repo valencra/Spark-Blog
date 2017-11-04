48ec6316e1cd93b6ed350c561dfbb799d21923d5
valencra <valencra@mcmaster.ca>
2017-11-04 16:00:52 -0400

    Updated BlogEntry and Comment models

--- src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
+++ src/main/java/com/teamtreehouse/blog/model/BlogEntry.java
@@ -1,8 +1,97 @@
 package com.teamtreehouse.blog.model;
 
+import com.github.slugify.Slugify;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class BlogEntry {
+    private String title;
+    private String body;
+    private Date date;
+    private List<Comment> comments;
+    private List<String> tags;
+    private String slug;
+
+    public BlogEntry(String title, String body) {
+        this.title = title;
+        this.body = body;
+        date = new Date();
+        comments = new ArrayList<>();
+        tags = new ArrayList<>();
+        try {
+            Slugify slugify = new Slugify();
+            slug = slugify.slugify(title);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
     public boolean addComment(Comment comment) {
-        // Store these comments!
-        return false;
+        return comments.add(comment);
+    }
+
+    public boolean addTag(String tag) {
+        return tags.add(tag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BlogEntry blogEntry = (BlogEntry) o;
+
+        if (title != null ? !title.equals(blogEntry.title) : blogEntry.title != null) return false;
+        if (body != null ? !body.equals(blogEntry.body) : blogEntry.body != null) return false;
+        if (date != null ? !date.equals(blogEntry.date) : blogEntry.date != null) return false;
+        if (comments != null ? !comments.equals(blogEntry.comments) : blogEntry.comments != null) return false;
+        if (tags != null ? !tags.equals(blogEntry.tags) : blogEntry.tags != null) return false;
+        return slug != null ? slug.equals(blogEntry.slug) : blogEntry.slug == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = title != null ? title.hashCode() : 0;
+        result = 31 * result + (body != null ? body.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + (comments != null ? comments.hashCode() : 0);
+        result = 31 * result + (tags != null ? tags.hashCode() : 0);
+        result = 31 * result + (slug != null ? slug.hashCode() : 0);
+        return result;
     }
 }
--- src/main/java/com/teamtreehouse/blog/model/Comment.java
+++ src/main/java/com/teamtreehouse/blog/model/Comment.java
@@ -1,4 +1,55 @@
 package com.teamtreehouse.blog.model;
 
+import java.util.Date;
+
 public class Comment {
+    private String author;
+    private String body;
+    private Date date;
+
+    public Comment(String author, String body) {
+        this.author = author;
+        this.body = body;
+        date = new Date();
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Comment comment = (Comment) o;
+
+        if (author != null ? !author.equals(comment.author) : comment.author != null) return false;
+        if (body != null ? !body.equals(comment.body) : comment.body != null) return false;
+        return date != null ? date.equals(comment.date) : comment.date == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = author != null ? author.hashCode() : 0;
+        result = 31 * result + (body != null ? body.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        return result;
+    }
 }
